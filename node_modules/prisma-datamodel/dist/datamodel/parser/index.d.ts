3145e6292e89d2f98dfc997dd5935bcd5eb846a1
brayoh <bkuriakb@gmail.com>
2019-01-06 17:39:34 +0300

    Remove node_modules

--- node_modules/prisma-datamodel/dist/datamodel/parser/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { DatabaseType } from '../../databaseType';
-import Parser from './parser';
-export default abstract class Parsers {
-    static create(databaseType: DatabaseType): Parser;
-}
