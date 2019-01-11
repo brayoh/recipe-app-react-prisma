fe794c012acedee6be13066c1d1cc6133b0828a3
brayoh <bkuriakb@gmail.com>
2019-01-11 15:54:52 +0300

    Add required folders

---
+++ client/src/components/AppHeader/AppHeader.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AppHeader = () => {
+  return <div />;
+};
+
+export default AppHeader;
---
+++ client/src/components/Card/Card.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Card = () => {
+  return <div />;
+};
+
+export default Card;
